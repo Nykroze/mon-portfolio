0b1d3e29ac9f9f0ba7d06a11ab90315a32f6d0a0
manu <manu23nde@gmail.com>
2024-12-15 17:01:49 +0100

    bug régler

--- todolist/script.js
+++ todolist/script.js
@@ -1,12 +1,11 @@
 let taskList= document.getElementById('taskList');
  console.log("c'est ici que ca ce passe");
 function addTask(){
-    
+
     console.log("c'est ici que ca ce passe");
     let taskInpunt =document.getElementById('taskInput')
     let taskText = taskInpunt.value;
       console.log(taskInpunt.value);
-   
     if(taskText===""){
         return
     }
@@ -19,30 +18,38 @@ function addTask(){
     editBtn.onclick =function (){
         editTask(li);
     };
+
+
     let deletebtn =document.createElement('button');
     deletebtn.innerHTML='<i class="bi bi-trash"></i>';
     deletebtn.onclick=function(){
         deleteTask(li);
     };
+
     li.appendChild(editBtn);
     li.appendChild(deletebtn);
     console.log("c'est ici que ca ce passe la");
     taskList.appendChild(li);
     taskInpunt.value="";
 }
-
 function editTask(task){
-    console.log("c'est ici que ca ce passe");
-    let taskTextElement= task.firstChild;
-    let taskText= taskTextElement.texContent;
-    let newTaskText= prompt('modifier la tache:', taskText)
 
+    console.log("c'est ici que ca ce passe");
+
+    let taskTextElement= task.firstChild;
+
+    let taskText= taskTextElement.textContent;
+
+    let newTaskText= prompt('modifier la tache:', taskText)
     if(newTaskText ===null|| newTaskText===""){
         return;
     }
-    taskTextElement.texContent=newTaskText;
+
+    taskTextElement.textContent=newTaskText;
+
 }
-
-
-
 console.log("c'est ici que ca ce passe");
+function deleteTask(task){
+    taskList.removeChild(task);
+
+}
